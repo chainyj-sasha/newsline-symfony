c51db55c017369358c8cd17b7035e2e374404e54
Alex <lg280683man1@yandex.ru>
2023-11-03 18:14:21 +0300

    Добавление пагинации на главной странице новостей  - Установка бандла knplabs/knp-paginator-bundle  - Рефакторинг метода index() ArticleController.php  - Рефакторинг article/index.html.twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\ArticleRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,9 +19,15 @@ class ArticleController extends AbstractController
     }
 
     #[Route('/', name: 'all_article')]
-    public function index(): Response
+    public function index(Request $request, PaginatorInterface $paginator): Response
     {
-        $articles = $this->articleRepository->findAll();
+        $query = $this->articleRepository->findAll();
+
+        $articles = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            10,
+        );
 
         return $this->render('article/index.html.twig', [
             'articles' => $articles,
