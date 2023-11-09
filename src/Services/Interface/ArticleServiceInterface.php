885ad431c6009e7932289002f155e1a619b25865
Alex <lg280683man1@yandex.ru>
2023-11-09 17:40:32 +0300

    Вынос логики из контроллеров в сервис  - Создал интерфейс ArticleServiceInterface.php  - Создал сервис ArticleService.php  - Перенес бизнес-логику из контролленов в сервис

---
+++ src/Services/Interface/ArticleServiceInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services\Interface;
+
+use App\Entity\Article;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+interface ArticleServiceInterface
+{
+    public function getAllArticles(Request $request, PaginatorInterface $paginator): PaginationInterface;
+
+    public function getOneArticle($id): Article;
+
+    public function articleStore(Request $request, $form);
+
+    public function deleteArticle(Article $article): void;
+
+    public function saveImage($image, Article $article): void;
+}
---
+++ src/Services/Service/ArticleService.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Services\Service;
+
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use App\Services\Interface\ArticleServiceInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class ArticleService implements ArticleServiceInterface
+{
+    private ArticleRepository $articleRepository;
+    private EntityManagerInterface $entityManager;
+
+    private KernelInterface $kernel;
+
+    public function __construct(ArticleRepository $articleRepository, EntityManagerInterface $entityManager, KernelInterface $kernel)
+    {
+        $this->articleRepository = $articleRepository;
+        $this->entityManager = $entityManager;
+        $this->kernel = $kernel;
+    }
+
+    public function getAllArticles(Request $request, PaginatorInterface $paginator): PaginationInterface
+    {
+        $query = $this->articleRepository->findAll();
+
+        return $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            10,
+        );
+    }
+
+    public function getOneArticle($id): Article
+    {
+        return $this->articleRepository->find($id);
+    }
+
+    public function articleStore($request, $form): bool
+    {
+        $flag = false;
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $article = $form->getData();
+
+                $image = $request->files->get('article')['image'];                    // получаем катринку из реквеста (article - это form_name)
+                if ($image) {
+                    $this->saveImage($image, $article);
+                }
+                $article->setViews(0);
+                $article->setCreatedAt(new \DateTime());
+
+                $this->entityManager->persist($article);
+                $this->entityManager->flush();
+
+                $flag = true;
+            }
+
+        return $flag;
+    }
+    public function deleteArticle(Article $article): void
+    {
+        $this->entityManager->remove($article);
+        $this->entityManager->flush();
+    }
+
+    public function saveImage($image, Article $article): void
+    {
+        $path = $this->kernel->getProjectDir() . '/public/uploads/images';                        // определяем место где будет храниться изображение '/public/uploads/images'
+        $imageName = 'uploads/images/' . uniqid() . $image->getClientOriginalName();              // Получаем оригинальное имя картинки
+        $image->move($path, $imageName);                                                          // Переносим картинку $imageName в папку $path
+        $article->setImage($imageName);
+    }
+
+
+}
